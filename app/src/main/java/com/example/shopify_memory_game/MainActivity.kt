6ec4aedd1620f016faab5aaf5fadb17ad8fd7a4a
const <constantin.cez.b@gmail.com>
2019-09-16 18:11:19 -0700

    Initial commit

---
+++ app/src/main/java/com/example/shopify_memory_game/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shopify_memory_game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
